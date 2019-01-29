06ced6047292db4c94430cfcef9b23ca3897877c
Somchai Somphadung <somchaisomph@gmail.com>
2019-01-29 11:38:42 +0700

    Create demo_training.py

---
+++ demos/demo_training.py
@@ -0,0 +1,17 @@
+import numpy as np
+from morning_glory import Config,FaceRecognizer
+
+conf = Config()
+conf.training_set_dir = '../../../orl_faces'
+conf.num_image = 8
+conf.image_subfix = 'pgm'
+conf.image_shape = [112,92]
+conf.output_size = 40
+conf.hidden_layers = 128
+conf.learning_rate = 0.001
+
+recognizer = FaceRecognizer()
+recognizer.set_config(conf)
+recognizer.init_parameters()
+recognizer.train(epoch=10,silence=False)
+recognizer.save_parameters("parameters.pickle")
