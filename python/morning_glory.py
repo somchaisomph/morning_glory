4fe0e1efcdb677186fc95ffbefc1e9d185e51b30
Somchai Somphadung <somchaisomph@gmail.com>
2019-05-31 22:01:42 +0700

    Update morning_glory.py

--- python/morning_glory.py
+++ python/morning_glory.py
@@ -4,7 +4,7 @@ import numpy as np
 import pickle
 # -- By Somchai Somphadung
 # -- Good idea from http://colah.github.io/posts/2015-08-Understanding-LSTMs/
-# -- updated Jan 27th 2019
+# -- updated May 31th 2019
 
 class Config():
     training_set_dir = '../faces/att/'
